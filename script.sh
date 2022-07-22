d126ef487bbb7d5f449498a469b6ab739c2dfbb3
leh-india <chaitu.silver@yahoo.com>
2022-07-22 21:19:02 +0530

    Modified script.sh file.

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd /opt/
+cd /var/lib/jenkins/workspace/python-webapp-deploy/
 
-FLASK_APP=/opt/app.py 
+chmod 777 script.sh
+
+FLASK_APP=/var/lib/jenkins/workspace/python-webapp-deploy/app.py
 
 nohup flask run --host=0.0.0.0 &
